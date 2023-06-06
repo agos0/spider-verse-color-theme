599a7ab0c7a6571fa962e9e5751cbc63fe839624
Aryan Goswami <agoswami1238@gmail.com>
2023-06-06 19:55:06 -0400

    added demos

---
+++ demo/demojs.js
@@ -0,0 +1,10 @@
+const studio = 'Sony';
+const movie = 'Spider-Verse'
+
+var spiderMan = 'Miles'
+
+function state() {
+    console.log(`${studio} made the ${movie} movies and ${spiderMan} is in it`);
+}
+
+state()
